e0bb35d6dbb4d3b41be0a34268fc585afb30ddbb
master <hemhm55@gmail.com>
2020-07-11 18:47:32 +0200

    added input

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -2,9 +2,30 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
 	render() {
+		const { item, handleChange, handleSumbit, editItem } = this.props;
 		return (
-			<div>
-				<h1>hi from todo list</h1>
+			<div className="card card-body my-3">
+				<form onSubmit={handleSumbit}>
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<div className="input-group-text bg-primary text-white">
+								<i className="fas fa-book"></i>
+							</div>
+						</div>
+						<input
+							type="text"
+							className="form-control text-capitalize"
+							placeholder="add todo items"
+							value={item}
+							onChange={handleChange}
+						/>
+					</div>
+					<button
+						type="sumbit"
+						className="btn btn-block btn-primary mt-3 text-uppercase">
+						add item
+					</button>
+				</form>
 			</div>
 		);
 	}
